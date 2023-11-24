a2e41cce3fe2a286c47af8230bd8fe456b76725d
CristiAndLook <krhistian1902@gmail.com>
2023-11-23 19:25:23 -0500

    GET and POST controllers Projects

--- src/app.js
+++ src/app.js
@@ -3,6 +3,10 @@ import projectsRoutes from './routes/projects.routes.js';
 
 const app = express();
 
+// middlewares
+app.use(express.json());
+
+
 app.use(projectsRoutes);
 
 export default app;
--- src/controllers/projects.controller.js
+++ src/controllers/projects.controller.js
@@ -1,7 +1,31 @@
-export const getProjects = (req, res) => {
-    res.send('GET PROJECTS');
+import { Project } from "../models/Project.js";
+
+export const getProjects = async (req, res) => {
+  try {
+    const projects = await Project.findAll();
+    res.json(projects);
+  } catch (error) {
+    return res.status(500).json({
+      message: "Something goes wrong",
+    });
+  }
 };
 
-export const createProject = (req, res) => {
-    res.send('CREATE PROJECT');
-}
+export const createProject = async (req, res) => {
+  try {
+    const { projectName, projectPriority, projectDescription } = req.body;
+
+  const newProject = await Project.create({
+    projectName,
+    projectPriority,
+    projectDescription,
+  });
+
+  res.json(newProject);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "Something goes wrong",
+    });
+  }
+};
